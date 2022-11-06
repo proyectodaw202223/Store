cf236d42aa907cc8f417378e22c2685d94f977f3
ANDER <romoander@gmail.com>
2022-11-06 17:20:39 +0100

    Orginial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import {  Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pimpoCreaciones';
+
+  public header_class: string = '';
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+
+    // Constantly check the url to set the header class.
+    this.router.events
+      .pipe(
+        filter((event: any) => event instanceof NavigationEnd)
+      )
+      .subscribe(event => {
+          if (event.urlAfterRedirects) {
+            const url = event.urlAfterRedirects;
+            if ((url === '/') )  {
+              this.header_class = 'home_header';
+            } else {
+              this.header_class = '';
+            }
+          }
+      });
+  }
+
 }
