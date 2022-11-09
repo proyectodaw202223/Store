6621a767d7bf77078f603ca2bf9ca4a18fbcbf8c
ANDER <romoander@gmail.com>
2022-11-09 18:26:07 +0100

    Creados componentes client y admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProductsComponent } from './products/products.component';
+import { AdminComponent } from './admin/admin.component';
+import { ClientRoutingModule } from './client/client-routing.module';
+import { ClientComponent } from './client/client.component';
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'products', component: ProductsComponent },
+  { path: '', component: ClientComponent },
+  { path: 'admin', component: AdminComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ClientRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
