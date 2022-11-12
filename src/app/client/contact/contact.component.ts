dcfb8e549a01d654c9f33bd7236312a3b4099a8c
romomartin <romoander@gmail.com>
2022-11-12 19:23:39 +0100

    Estilos definidos en about y contacto

--- src/app/client/contact/contact.component.ts
+++ src/app/client/contact/contact.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: [
+    './contact.component.css',
+    '../client.component.css'
+    ]
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  name: string = '';
+  surname: string = '';
+  email: string = '';
+  message: string = '';
+
+  constructor(private contact: ContactService) { }
 
   ngOnInit(): void {
+  }
+  submitForm(){
+    postMessage(this.message);
   }
 
 }
