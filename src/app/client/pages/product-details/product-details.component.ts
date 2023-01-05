76fcc67d8f5cd5a6bc3dfea83b1565880c98ca45
romomartin <romoander@gmail.com>
2023-01-05 09:57:23 +0100

    product-details wip

--- src/app/client/pages/product-details/product-details.component.ts
+++ src/app/client/pages/product-details/product-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductItemImage } from 'src/app/models/productItemImage.model';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  public newProducts: Product[] = [];
+  public product: Product = <Product>{}; 
+  public apiStorage: string = environment.apiStorage;
+  public productImages: ProductItemImage[] = []
+
+  constructor(
+    private _productService: ProductService,
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+
+    // Populate product and productImages
+    const id = Number(this._route.snapshot.paramMap.get('id'));
+    this._productService.getProductById(id).subscribe({
+      next: (result) => {
+        this.product = result;
+        for (let item of this.product.productItems){
+          if ( item.images !== undefined){
+            for (let image of item.images){
+              this.productImages.push(image);
+            }
+          }
+        }
+        console.log(this.productImages);
+        return result;
+      },
+      error: (error) => {
+        console.log(<any>error);
+        return error;
+      }
+    })
+    // Populate newProducts
+    this._productService.getNewProducts(5).subscribe({
+      next: (result) => {
+        this.newProducts = result;
+        return result;
+      },
+      error: (error) => {
+        console.log(<any>error);
+        return error;
+      }
+    })
+    
   }
 
 }
