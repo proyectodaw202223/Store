d6641f679fad15358f246aa5a0259bbac891c1e3
afidalgobirt <afidalgo@birt.eus>
2023-01-07 16:09:34 +0100

    GUI Implemented

--- src/app/admin/pages/detproductos/detproductos.component.ts
+++ src/app/admin/pages/detproductos/detproductos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detproductos',
   templateUrl: './detproductos.component.html',
-  styleUrls: ['./detproductos.component.css']
+  styleUrls: ['./detproductos.component.css', '../../admin.component.css']
 })
 export class DetproductosComponent implements OnInit {
 
